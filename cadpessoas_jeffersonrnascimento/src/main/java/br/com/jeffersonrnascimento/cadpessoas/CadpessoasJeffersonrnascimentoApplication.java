8f83fbc2fb443ff57a49e12249387f086a75bfe0
Jefferson Nascimento <jefferson.nascimento@alunos.ufersa.edu.br>
2021-03-31 20:04:27 -0300

    Iniciando CRUD da Unidade 1

---
+++ cadpessoas_jeffersonrnascimento/src/main/java/br/com/jeffersonrnascimento/cadpessoas/CadpessoasJeffersonrnascimentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.jeffersonrnascimento.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasJeffersonrnascimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasJeffersonrnascimentoApplication.class, args);
+	}
+
+}
