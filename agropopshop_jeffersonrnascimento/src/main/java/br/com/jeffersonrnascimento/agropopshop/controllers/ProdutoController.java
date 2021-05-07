a8daeb1fbaefefa82d6039aa2e577011417bde8f
Jefferson Nascimento <jefferson.nascimento@alunos.ufersa.edu.br>
2021-05-07 00:56:29 -0300

    Atualizando o UserStory 1, 2

--- agropopshop_jeffersonrnascimento/src/main/java/br/com/jeffersonrnascimento/agropopshop/controllers/ProdutoController.java
+++ agropopshop_jeffersonrnascimento/src/main/java/br/com/jeffersonrnascimento/agropopshop/controllers/ProdutoController.java
@@ -7,64 +7,66 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.jeffersonrnascimento.agropopshop.model.Produto;
 import br.com.jeffersonrnascimento.agropopshop.repositories.ProdutoRepository;
 
 @Controller
+@RequestMapping("/")
 public class ProdutoController {
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepo;
-	
-	ProdutoController (ProdutoRepository produtoRepo){
+
+	ProdutoController(ProdutoRepository produtoRepo) {
 		this.produtoRepo = produtoRepo;
 	}
-	
-	@GetMapping("/listarProdutos")
+
+	@GetMapping("/{listarProdutos}")
 	public ModelAndView listarProdutos() {
 		List<Produto> lista = produtoRepo.findAll();
-		ModelAndView ModelAndView = new ModelAndView("/listarProdutos");
+		ModelAndView ModelAndView = new ModelAndView("listarProdutos");
 		ModelAndView.addObject("produtos", lista);
 		return ModelAndView;
 	}
-	
+
 	@GetMapping("/adicionarProduto")
 	public ModelAndView adicionarProduto() {
 		ModelAndView mav = new ModelAndView("/adicionarProduto");
 		mav.addObject(new Produto());
 		return mav;
 	}
-	
+
 	@PostMapping("/adicionarProduto")
 	public String adicionarProduto(Produto p) {
 		this.produtoRepo.save(p);
 		return "redirect:/listarProdutos";
 	}
-	
+
 	@GetMapping("/removerProduto/{id}")
-	public ModelAndView removerProduto(@PathVariable("id") long id){
+	public ModelAndView removerCliente(@PathVariable("id") long id) {
 		Produto aRemover = produtoRepo.findById(id)
-		.orElseThrow(() -> new IllegalArgumentException("ID inválido:" + id));
-		
+				.orElseThrow(() -> new IllegalArgumentException("ID inválido:" + id));
+
 		produtoRepo.delete(aRemover);
 		return new ModelAndView("redirect:/listarProdutos");
 	}
-	
+
 	@GetMapping("/editarProduto/{id}")
-	public ModelAndView editarProduto (@PathVariable("id") long id) {
+	public ModelAndView editarProduto(@PathVariable("id") long id) {
 		Produto produto = produtoRepo.findById(id)
 				.orElseThrow(() -> new IllegalArgumentException("ID inválido:" + id));
-		
+
 		ModelAndView ModelAndView = new ModelAndView("/editarProduto");
 		ModelAndView.addObject(produto);
 		return ModelAndView;
 	}
-	
+
 	@PostMapping("/editarProduto/{id}")
-	public ModelAndView editarProduto(@PathVariable("id")long id, Produto produto) {
+	public ModelAndView editarProduto(@PathVariable("id") long id, Produto produto) {
 		this.produtoRepo.save(produto);
-		return new ModelAndView( "redirect:/listarProdutos");
+		return new ModelAndView("redirect:/listarProdutos");
 	}
 }
--- agropopshop_jeffersonrnascimento/src/main/resources/import.sql
+++ agropopshop_jeffersonrnascimento/src/main/resources/import.sql
@@ -1,5 +1,7 @@
 ﻿/*Primeira linha em branco - Spring não tava adicionado os valores*/
 INSERT INTO clientes (nome, email, genero, telefone, uf, cep, cidade, bairro, rua, numero) VALUES ('Joana Darck', 'joana@gmail.com', 'Feminino', '(83) 99443-5621', 'SP', '90.743-231', 'Monte Alegre', 'Lagoa Nova', 'Rua Das Graças', '80');
 INSERT INTO clientes (nome, email, genero, telefone, uf, cep, cidade, bairro, rua, numero) VALUES ('Pabllo Vittar', 'vittar@oi.com', 'Masculino', '(84) 4213-5321', 'RJ', '40.213-642', 'Rio de Janeiro', 'Lagoa Seca', 'Rua Sra. do Destino', '11');
-INSERT INTO produtos (nomeProduto, marca, preco, altura, largura, profundidade, peso) VALUES ('Castanha', 'Angicos', 7.00, 1.55, 0.10, 0.05, 5.00);
+INSERT INTO clientes (nome, email, genero, telefone, uf, cep, cidade, bairro, rua, numero) VALUES ('Pabllo Vittar', 'vittar@oi.com', 'Masculino', '(84) 4213-5321', 'RJ', '40.213-642', 'Rio de Janeiro', 'Lagoa Seca', 'Rua Sra. do Destino', '11');
+INSERT INTO clientes (nome, email, genero, telefone, uf, cep, cidade, bairro, rua, numero) VALUES ('Pabllo Vittar', 'vittar@oi.com', 'Masculino', '(84) 4213-5321', 'RJ', '40.213-642', 'Rio de Janeiro', 'Lagoa Seca', 'Rua Sra. do Destino', '11');
+INSERT INTO produtos (nomeProduto, marca, preco, altura, largura, profundidade, peso) VALUES ('Castanha', 'Angicos', 7.50, 1, 10, 11, 5);
 
