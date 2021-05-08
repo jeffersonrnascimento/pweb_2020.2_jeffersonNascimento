d232c1b530a613d2a505029fcf411a64fb6649ee
Jefferson Nascimento <jefferson.nascimento@alunos.ufersa.edu.br>
2021-05-08 14:50:11 -0300

    Finalizado o UserStory 1, 2

--- agropopshop_jeffersonrnascimento/src/main/resources/import.sql
+++ agropopshop_jeffersonrnascimento/src/main/resources/import.sql
@@ -3,6 +3,9 @@ INSERT INTO clientes (nome, email, genero, telefone, uf, cep, cidade, bairro, ru
 INSERT INTO clientes (nome, email, genero, telefone, uf, cep, cidade, bairro, rua, numero) VALUES ('Pabllo Vittar', 'vittar@oi.com', 'Masculino', '(84) 4213-5321', 'RJ', '40.213-642', 'Rio de Janeiro', 'Lagoa Seca', 'Rua Sra. do Destino', '11');
 INSERT INTO clientes (nome, email, genero, telefone, uf, cep, cidade, bairro, rua, numero) VALUES ('Pabllo Vittar', 'vittar@oi.com', 'Masculino', '(84) 4213-5321', 'RJ', '40.213-642', 'Rio de Janeiro', 'Lagoa Seca', 'Rua Sra. do Destino', '11');
 INSERT INTO clientes (nome, email, genero, telefone, uf, cep, cidade, bairro, rua, numero) VALUES ('Pabllo Vittar', 'vittar@oi.com', 'Masculino', '(84) 4213-5321', 'RJ', '40.213-642', 'Rio de Janeiro', 'Lagoa Seca', 'Rua Sra. do Destino', '11');
-INSERT INTO produtos (nomeProduto, marca, preco, altura, largura, profundidade, peso) VALUES ('Castanha', 'Angicos', 7.50, 1, 10, 11, 5);
+INSERT INTO produtos (nome_produto, marca, preco, altura, largura, profundidade, peso) VALUES ('Castanha', 'Angicos', 7.50, 1, 10, 11, 5);
+INSERT INTO produtos (nome_produto, marca, preco, altura, largura, profundidade, peso) VALUES ('Amendoin', 'Assú', 10.50, 0.50, 0.50, 3.00, 200);
+INSERT INTO produtos (nome_produto, marca, preco, altura, largura, profundidade, peso) VALUES ('Arroz', 'Jucurutu', 20.00, 17, 10, 15, 1);
+INSERT INTO produtos (nome_produto, marca, preco, altura, largura, profundidade, peso) VALUES ('Castanha', 'Angicos', 7.50, 1, 10, 11, 5);
 INSERT INTO dependentes (nomeCompleto, dataNascimento, genero) VALUES ('Luiz Inácio', parseDateTime('15/12/1975','dd/MM/yyyy'), 'Masculino');
 
